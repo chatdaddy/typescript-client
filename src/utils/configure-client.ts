f9e86489b707716323a6c90415bdd15c2d63f63d
Adhiraj Singh <adhirajsingh1001@gmail.com>
2023-07-12 10:12:49 +0530

    fix: exponential delay params

--- src/utils/configure-client.ts
+++ src/utils/configure-client.ts
@@ -1,3 +1,6 @@
+/**
+ * Configure the default client to retry requests on failure
+ */
 import axios from "axios";
 import axiosRetry from "axios-retry";
 import { BaseAPI } from "../OpenAPI/base";
@@ -9,8 +12,12 @@ export const CLIENT_AXIOS = axios.create()
 axiosRetry(
 	CLIENT_AXIOS,
 	{
+		// max number of retries
 		retries: 3,
-		retryDelay: axiosRetry.exponentialDelay,
+		retryDelay: (...args) => (
+			// retry after 500ms, 1000ms, 2000ms, 4000ms, 8000ms
+			axiosRetry.exponentialDelay(...args, 500)
+		),
 		retryCondition: (err) => {
 			return (
 				axiosRetry.isNetworkError(err) 
