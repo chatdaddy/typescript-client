3735c4f4a434d9d1d096fbce172fbe3a5ed7ed8f
Adhiraj Singh <adhirajsingh1001@gmail.com>
2023-10-04 15:06:27 +0530

    docs: add auto charge example

--- examples/purchase-account.ts
+++ examples/purchase-account.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { makeAccessTokenFactory, AccountApi, AccountTier, AccountType, Configuration } from '../src'
+import { makeAccessTokenFactory, AccountApi, AccountTier, AccountType, Configuration, TeamDetailApi } from '../src'
 
 (async() => {
 	const { REFRESH_TOKEN, TEAM_ID } = process.env
@@ -14,7 +14,11 @@ import { makeAccessTokenFactory, AccountApi, AccountTier, AccountType, Configura
 	})
 
 	const { token: accessToken } = await getAccessToken(TEAM_ID)
+
+	await ensureAutoChargeEnabled()
+
 	const accountsApi = new AccountApi(new Configuration({ accessToken }))
+	const accountTier = AccountTier.LimitedMsgNoChatHistory
 	const { data } =  await accountsApi.accountsPost({
 		accountsPostRequest: {
 			// just a nickname -- does not need to be unique
@@ -22,8 +26,8 @@ import { makeAccessTokenFactory, AccountApi, AccountTier, AccountType, Configura
 			nickname: 'My New Account',
 			// change the tier to upgrade/downgrade functionality
 			// this one gives only limited messages, and syncs no chat history
-			tier: AccountTier.LimitedMsgNoChatHistory,
-			type: AccountType.Wa
+			tier: accountTier,
+			type: 'wa'
 		}
 	})
 
@@ -51,5 +55,23 @@ import { makeAccessTokenFactory, AccountApi, AccountTier, AccountType, Configura
 				tier: AccountTier.UnlimitedMsgChatHistory
 			}
 		})
+	}
+
+	/// Ensures that auto charge for new accounts is enabled
+	/// this allows CD to automatically charge your card
+	/// for any new accounts you create
+	async function ensureAutoChargeEnabled() {
+		const teamDetailApi = new TeamDetailApi(new Configuration({ accessToken }))
+		const { data } = await teamDetailApi.teamDetailGet()
+		const accountTier = 'limitedMsgNoHistoryAccountDay'
+		if(!data.autoChargeItems.includes(accountTier)) {
+			await teamDetailApi.teamDetailPatch({
+				teamDetailUpdate: {
+					autoChargeItems: new Set([...data.autoChargeItems, accountTier])
+				}
+			})
+
+			console.log('auto charge enabled!')
+		}
 	}
 })()
