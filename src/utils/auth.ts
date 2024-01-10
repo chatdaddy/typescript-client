42a487b081ddbf1c2020ad555b4ca9a0e14e7f3c
Adhiraj Singh <adhirajsingh1001@gmail.com>
2024-01-10 12:45:46 +0530

    chore: allow customising token expiry interval

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -9,9 +9,6 @@ MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEevVHEB81+mIuHJ6Ka2+GveuyAb2P
 SNEGnm4K1V6HzZF0F9+mQS7N0UHNE+gv0OQIKi5D6e48ZCVytj3iX4Todg==
 -----END PUBLIC KEY-----
 `
-const RETRY_WAIT_INTERVAL_MS = 500
-// expire the token a little early to account for delays in network requests
-const TOKEN_EXPIRY_MARGIN_MS = 60 * 1000 // 1 minute
 
 /** exports the binary string used in auth tokens */
 export const generateBinaryString = (scopes: Scope[]) => {
@@ -39,6 +36,7 @@ export const verifyToken = (token: string) => {
 	const user = verifyJWT(token, PUBLIC_KEY, { algorithms: ['ES256'] }) as JWT
 	return user
 }
+
 /** decodes a JWT token and returns the included object */
 export const decodeToken = (token: string) => {
 	const comps = token.split('.')
@@ -47,6 +45,7 @@ export const decodeToken = (token: string) => {
 }
 /** get the expiry date of a JWT token */
 export const expiryDateOfToken = (jwt: JWT) => (new Date(jwt.exp * 1000))
+
 /** get the scopes from a binary encoded string */
 export const getScopes = (binary: string) => {
 	const scopes: Scope[] = []
@@ -68,6 +67,7 @@ export const validateUserScopes = (user: JWT, ...scopes: Scope[]) => {
 	const authorized = missingScopes.length < scopes.length
 	return { authorized, missingScopes }
 }
+
 /** Options to create an access token for use in APIs */
 export type AccessTokenFactoryOptions = {
 	/** extra parameters like scopes to pass to the token generation request */
@@ -78,13 +78,30 @@ export type AccessTokenFactoryOptions = {
 	config?: ConfigurationParameters
 	/** max number of retries, 500ms delay between requests */
 	maxRetries?: number 
+	/**
+	 * margin in milliseconds to expire the token early
+	 * to account for delays in network requests
+	 * @default 2m
+	 */
+	tokenExpiryMarginMs?: number
+}
+
+type TokenCacheItem = {
+	token: Promise<string | { error: Error }>
+	expiresAt: Date | undefined
 }
 
 export const makeAccessTokenFactory = (
-	{ request, existingTokens, config, maxRetries }: AccessTokenFactoryOptions
+	{
+		request,
+		existingTokens,
+		config,
+		maxRetries,
+		tokenExpiryMarginMs = 2 * 60_000
+	}: AccessTokenFactoryOptions
 ) => {
 
-	type TokenCache = { [_: string]: { token: Promise<string | { error: Error }>, expiresAt: Date | undefined } }
+	type TokenCache = { [_: string]: TokenCacheItem }
 
 	existingTokens = existingTokens || []
 	maxRetries = maxRetries || 1
@@ -93,43 +110,45 @@ export const makeAccessTokenFactory = (
 	const tokenCache: TokenCache = 
 		existingTokens.reduce((dict, token) => {
 			const jwt = decodeToken(token)
-			const expiresAt = new Date(expiryDateOfToken(jwt).getTime() - TOKEN_EXPIRY_MARGIN_MS)
+			const expiresAt = new Date(
+				expiryDateOfToken(jwt).getTime()
+					- tokenExpiryMarginMs
+			)
 			if(expiresAt.getTime() > Date.now()) {
-				dict[jwt.user.teamId] = { token: Promise.resolve(token), expiresAt }
+				dict[jwt.user.teamId] = {
+					token: Promise.resolve(token),
+					expiresAt
+				}
 			}
 			return dict
 		}, {} as TokenCache)
 
 	const makeTokenApiRequest = async(req: RefreshTokenLoginRequest) => {
-		let triesLeft = maxRetries
-		while(true) {
-			try {
-				const result = await tokenAPI.tokenPost({ authRequest: req })
-				return result
-			} catch(error) {
-				triesLeft -= 1
-				// throw the error if it fails
-				if(triesLeft <= 0) {
-					throw error
-				}
-				// wait some time before retrying
-				await new Promise(resolve => setTimeout(resolve, RETRY_WAIT_INTERVAL_MS))
-			}
-		}
+		return tokenAPI.tokenPost({ authRequest: req })
 	}
 
 	return async (teamId: string) => {
 		const key = teamId
 		let task = tokenCache[key]
-		if(!task || (!!task.expiresAt && (task.expiresAt?.getTime() < Date.now()))) {
+		if(
+			!task
+			|| (
+				!!task.expiresAt
+				&& (task.expiresAt?.getTime() < Date.now())
+			)
+		) {
 			tokenCache[key] = {
 				token: (async() => {
 					try {
 						const { data: { access_token } } = await makeTokenApiRequest(
 							{ ...request, teamId }
 						)
+
 						const jwt = decodeToken(access_token)
-						const expiresAt = new Date(expiryDateOfToken(jwt).getTime() - TOKEN_EXPIRY_MARGIN_MS)
+						const expiresAt = new Date(
+							expiryDateOfToken(jwt).getTime()
+							- tokenExpiryMarginMs
+						)
 						if(tokenCache[key]) {
 							tokenCache[key]!.expiresAt = expiresAt
 						}
